851796bf0800546ad79155c70086e3c6ec7f491b
Tuncay Yavuz Büyükadalı <76534892+Yavuz1234567890@users.noreply.github.com>
2021-06-11 16:13:53 +0300

    For cringe

--- Cpp/UltimateCringe.cpp
+++ Cpp/UltimateCringe.cpp
@@ -1,2 +1,8 @@
-#include<iostream>
-int main(int argc,char**argv){std::cout<<"Hello World!"<<std::endl;return 0;}
+#include <iostream>
+
+int main(int argc,char**argv){
+  for(int i = 0; i < 10; i++){
+    std::cout<<"Hello World!"<<std::endl;
+  }
+  return 0;
+}
